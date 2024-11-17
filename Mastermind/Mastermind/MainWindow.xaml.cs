f7ab3a3ef24efe108b81279928cc46af9bc1fac1
dorifrogg <12104047@student.pxl.be>
2024-11-17 20:25:36 +0100

    MasterMind-02 & MasterMind-03 Code & ComboBoxen

--- Mastermind/Mastermind/MainWindow.xaml.cs
+++ Mastermind/Mastermind/MainWindow.xaml.cs
@@ -16,17 +16,56 @@ namespace Mastermind
     /// </summary>
     public partial class MainWindow : Window
     {
+        string colour1;
+        string colour2;
+        string colour3;
+        string colour4;
         public MainWindow()
         {
             InitializeComponent();
-            int nummer1;
-            nummer1 = GenerateColour();
+            GenerateColours(out colour1, out colour2, out colour3, out colour4);
+            Title += $": ({colour1}, {colour2}, {colour3}, {colour4})";
         }
 
-        public int GenerateColour()
+        public void GenerateColours(out string colourSlot1, out string colourSlot2, out string colourSlot3, out string colourSlot4)
         {
             Random rng = new Random();
-            return rng.Next(1, 7);
+            List<string> colourList = new List<string>();
+            int rngNumber;
+            string rngString = "";
+            
+            for (int i=0; i<4; i++)
+            {
+                rngNumber = rng.Next(1, 7);
+                switch (rngNumber)
+                {
+                    case 1:
+                        rngString = "Red";
+                        break;
+                    case 2:
+                        rngString = "Yellow";
+                        break;
+                    case 3:
+                        rngString = "Orange";
+                        break;
+                    case 4:
+                        rngString = "White";
+                        break;
+                    case 5:
+                        rngString = "Green";
+                        break;
+                    case 6:
+                        rngString = "Blue";
+                        break;
+                }
+                colourList.Add(rngString);
+            }
+            colourSlot1 = colourList[0];
+            colourSlot2 = colourList[1];
+            colourSlot3 = colourList[2];
+            colourSlot4 = colourList[3];
         }
+
+        
     }
 }
