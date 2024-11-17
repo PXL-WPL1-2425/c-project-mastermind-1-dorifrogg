a7955f1f1e4cc0da19cb86a0749219608de78a12
dorifrogg <12104047@student.pxl.be>
2024-11-17 21:17:49 +0100

    MasterMind-04 Kleur kiezen

--- Mastermind/Mastermind/MainWindow.xaml.cs
+++ Mastermind/Mastermind/MainWindow.xaml.cs
@@ -27,6 +27,7 @@ namespace Mastermind
             Title += $": ({colour1}, {colour2}, {colour3}, {colour4})";
         }
 
+        //METHODS
         public void GenerateColours(out string colourSlot1, out string colourSlot2, out string colourSlot3, out string colourSlot4)
         {
             Random rng = new Random();
@@ -66,6 +67,118 @@ namespace Mastermind
             colourSlot4 = colourList[3];
         }
 
-        
+
+        //EVENT METHODS
+        private void comboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBox1.SelectedIndex != -1)
+            {
+                switch (comboBox1.SelectedIndex)
+                {
+                    case 0:
+                        colour1Label.Background = Brushes.Red;
+                        break;
+                    case 1:
+                        colour1Label.Background = Brushes.Yellow;
+                        break;
+                    case 2:
+                        colour1Label.Background = Brushes.Orange;
+                        break;
+                    case 3:
+                        colour1Label.Background = Brushes.White;
+                        break;
+                    case 4:
+                        colour1Label.Background = Brushes.Green;
+                        break;
+                    case 5:
+                        colour1Label.Background = Brushes.Blue;
+                        break;
+                }
+            }
+        }
+
+        private void comboBox2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBox2.SelectedIndex != -1)
+            {
+                switch (comboBox2.SelectedIndex)
+                {
+                    case 0:
+                        colour2Label.Background = Brushes.Red;
+                        break;
+                    case 1:
+                        colour2Label.Background = Brushes.Yellow;
+                        break;
+                    case 2:
+                        colour2Label.Background = Brushes.Orange;
+                        break;
+                    case 3:
+                        colour2Label.Background = Brushes.White;
+                        break;
+                    case 4:
+                        colour2Label.Background = Brushes.Green;
+                        break;
+                    case 5:
+                        colour2Label.Background = Brushes.Blue;
+                        break;
+                }
+            }
+        }
+
+        private void comboBox3_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBox3.SelectedIndex != -1)
+            {
+                switch (comboBox3.SelectedIndex)
+                {
+                    case 0:
+                        colour3Label.Background = Brushes.Red;
+                        break;
+                    case 1:
+                        colour3Label.Background = Brushes.Yellow;
+                        break;
+                    case 2:
+                        colour3Label.Background = Brushes.Orange;
+                        break;
+                    case 3:
+                        colour3Label.Background = Brushes.White;
+                        break;
+                    case 4:
+                        colour3Label.Background = Brushes.Green;
+                        break;
+                    case 5:
+                        colour3Label.Background = Brushes.Blue;
+                        break;
+                }
+            }
+        }
+
+        private void comboBox4_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBox4.SelectedIndex != -1)
+            {
+                switch (comboBox4.SelectedIndex)
+                {
+                    case 0:
+                        colour4Label.Background = Brushes.Red;
+                        break;
+                    case 1:
+                        colour4Label.Background = Brushes.Yellow;
+                        break;
+                    case 2:
+                        colour4Label.Background = Brushes.Orange;
+                        break;
+                    case 3:
+                        colour4Label.Background = Brushes.White;
+                        break;
+                    case 4:
+                        colour4Label.Background = Brushes.Green;
+                        break;
+                    case 5:
+                        colour4Label.Background = Brushes.Blue;
+                        break;
+                }
+            }
+        }
     }
 }
